04a73954788ccd1ec9559fe39fdd6d8d8a60a92f
kwang-sub <chlrhkdtjq11@naver.com>
2022-06-17 18:00:01 +0900

    켄트백-TDD 읽으면서 작성한 코드 TDD를 활용하는 방법 같은 내용의 책이였다. 아직 부족하여 해당 내용들을 바로 적용 불가능하다는 생각이 들어 최범균-테스트 주도 개발 시작하기로 추가학습 진행 후 켄트백-TDD 다시 읽어보기

--- TDD/TDD/src/main/java/money/Money.java
+++ TDD/TDD/src/main/java/money/Money.java
@@ -1,21 +1,49 @@
 package money;
 
-public abstract class Money {
+public  class Money implements Expression {
     protected int amount;
+    protected String currency;
 
-    static Dollar dollar(int amount) {
-        return new Dollar(amount);
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
     }
 
-    static Franc franc(int amount) {
-        return new Franc(amount);
+    static Money dollar(int amount) {
+        return new Money(amount, "USD");
+    }
+
+    static Money franc(int amount) {
+        return new Money(amount, "CHF");
     }
 
     @Override
     public boolean equals(Object obj) {
         Money money = (Money) obj;
-        return amount == money.amount && getClass().equals(money.getClass());
+        return amount == money.amount && currency().equals(money.currency());
     }
 
-    abstract Money times(int multiplier);
+    @Override
+    public Expression times(int multiplier) {
+        return new Money(amount * multiplier, currency);
+    };
+    String currency(){
+        return currency;
+    };
+    public Expression plus(Expression addend) {
+        return new Sum(this, addend);
+    }
+    @Override
+    public Money reduce(Bank bank, String to) {
+        int rate = bank.rate(currency, to);
+        return new Money(amount / rate, to);
+    }
+
+    @Override
+    public String toString() {
+        return "Money{" +
+                "amount=" + amount +
+                ", currency='" + currency + '\'' +
+                '}';
+    }
 }
