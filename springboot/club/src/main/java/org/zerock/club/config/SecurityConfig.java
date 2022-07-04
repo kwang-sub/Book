75887b82a9fff97849dcbddbe85aa03be967457d
kwang-sub <chlrhkdtjq11@naver.com>
2022-07-04 23:35:41 +0900

    git 수정중

---
+++ springboot/club/src/main/java/org/zerock/club/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package org.zerock.club.config;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@Log4j2
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .antMatchers("/sample/all").permitAll()
+                .antMatchers("/sample/member").hasRole("USER");
+
+        http.formLogin();
+        http.csrf().disable();
+        http.oauth2Login();
+
+        return http.build();
+    }
+
+}
--- springboot/mreview/src/main/java/org/zerock/mreview/mreview/controller/UploadController.java
+++ springboot/mreview/src/main/java/org/zerock/mreview/mreview/controller/UploadController.java
@@ -64,12 +64,16 @@ public class UploadController {
     }
 
     @GetMapping("/display")
-    public ResponseEntity<byte[]> getFile(String fileName) {
+    public ResponseEntity<byte[]> getFile(String fileName, String size) {
         ResponseEntity<byte[]> result = null;
         try {
             String srcFileName = URLDecoder.decode(fileName, "UTF-8");
             log.info("fileName" + srcFileName);
             File file = new File(uploadPath + File.separator + srcFileName);
+            if (size != null && size.equals("1")) {
+                file = new File(file.getParent(), file.getName().substring(2));
+                log.info(":--------------------------"+file.getParent());
+            }
             log.info("file: " + file);
             HttpHeaders headers = new HttpHeaders();
             headers.add("Content-Type", Files.probeContentType(file.toPath()));
