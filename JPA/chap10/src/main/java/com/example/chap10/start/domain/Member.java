6bf29948652f2092ffdd6899349b1cd45a628bcb
kwang-sub <chlrhkdtjq11@naver.com>
2022-09-06 23:17:30 +0900

    QueryDSL 학습중

---
+++ JPA/chap10/src/main/java/com/example/chap10/start/domain/Member.java
@@ -0,0 +1,49 @@
+package com.example.chap10.start.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Setter
+@Getter
+@NamedQueries({
+        @NamedQuery(name = "Member.findByUsername",
+                query = "select m from Member m where m.name = :name"),
+        @NamedQuery(name = "Member.count",
+                query = "select count (m) from Member m")
+})
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    private String name;
+    private int age;
+
+    @OneToMany(mappedBy ="member")
+    private List<Order> orders = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "TEMA_ID")
+    private Team team;
+
+    public void setTeam(Team team) {
+        team.getMembers().add(this);
+        this.team = team;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", orders=" + orders +
+                '}';
+    }
+}
