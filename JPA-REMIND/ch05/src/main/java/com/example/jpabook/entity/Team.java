3875b651932e5afe5a9f7cfee0a23ebc36330865
kwang-sub <74045087+kwang-sub@users.noreply.github.com>
2022-12-05 22:43:50 +0900

    JPA 복습 다시 시작~

--- JPA-REMIND/ch05/src/main/java/com/example/jpabook/entity/Team.java
+++ JPA-REMIND/ch05/src/main/java/com/example/jpabook/entity/Team.java
@@ -6,15 +6,24 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
 public class Team {
 
     @Id
     @Column(name = "TEAM_ID")
     private String id;
     private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
+    public Team(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
--- JPA-REMIND/ch05/src/main/java/com/example/jpabook/main/Main.java
+++ JPA-REMIND/ch05/src/main/java/com/example/jpabook/main/Main.java
@@ -7,6 +7,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class Main {
 
@@ -16,9 +17,40 @@ public class Main {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         testSave(em);
+        updateRelation(em);
+        deleteRelation(em);
+//        testFind(em);
+//        queryLogicJoin(em);
+
         tx.commit();
         em.close();
         emf.close();
+    }
+
+    private static void deleteRelation(EntityManager em) {
+        Member member2 = em.getReference(Member.class, "member2");
+        member2.setTeam(null);
+    }
+
+    private static void updateRelation(EntityManager em) {
+        Team team2 = new Team("team2", "팀2");
+        em.persist(team2);
+        Member member = em.getReference(Member.class, "member1");
+        member.setTeam(team2);
+    }
+
+    private static void queryLogicJoin(EntityManager em) {
+        String jpql = "select m from Member m join m.team t where t.name =: teamName";
+        List<Member> resultList = em.createQuery(jpql, Member.class)
+                .setParameter("teamName", "팀1")
+                .getResultList();
+        resultList.forEach(member -> System.out.println(member.getUsername()));
+    }
+
+    private static void testFind(EntityManager em) {
+        Member findMember = em.find(Member.class, "member1");
+        Team findMemberTeam = findMember.getTeam();
+        System.out.println("찾은 회원 : " + findMember.getUsername() + " 팀은?" + findMemberTeam.getName());
     }
 
     private static void testSave(EntityManager em) {
@@ -28,5 +60,9 @@ public class Main {
         Member member1 = new Member("member1", "회원1");
         member1.setTeam(team1);
         em.persist(member1);
+
+        Member member2 = new Member("member2", "회원2");
+        member2.setTeam(team1);
+        em.persist(member2);
     }
 }
